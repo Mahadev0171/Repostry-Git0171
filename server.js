801a23c521c28ab6f3376a6974ddb464fe6eb619
unknown <maha.gowda0171@gmail.com>
2020-01-06 16:31:37 +0530

    second commit1

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.get('/*', function(req,res) {
     
 res.sendFile(path.join(__dirname));
 });
+console.log("data");
 
 // Start the app by listening on the default Heroku port
 const server = http.createServer(app);
